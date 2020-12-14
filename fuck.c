5d72be30cb6b7738eacf23f36b3a347a25022c8e
eir457882 <eir457882@gmail.com>
2020-12-14 16:12:56 +0800

    fuck.c

---
+++ fuck.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main( ){
+
+ FILE *fp;
+ char ch;
+
+  printf("lanpa");
+
+  if((fp = fopen("test.txt","r"))== NULL)
+  {			
+         printf("open file fuck\n");
+         system("PAUSE");
+         exit(0);
+  }
+
+  printf("lanpa2");
+
+  while( (ch=getc(fp) != EOF ))
+  {
+	 int i = 0; 
+         printf("LOOP%C\n",ch);
+	 printf("conunt %d\n",i);
+	 i++;
+  }		
+			
+  fclose(fp);	
+  return 0;
+	
+}
